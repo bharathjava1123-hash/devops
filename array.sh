79fc3a44b70c0c66b1d08d583e7af1e241bca7ae
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-03-26 12:59:00 +0530

    name

---
+++ array.sh
@@ -0,0 +1,7 @@
+names=("bharath","Yamini","Samyuktha")
+
+echo "first name is : ${names[0]}"
+echo "second name is : ${names[1]}"
+echo "third name is : ${names[2]}"
+
+echo "all names is : ${names[@]}"
