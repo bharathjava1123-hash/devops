3ff6fdf884926209ad02705fa94eef03f0d3655e
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-03-28 08:18:48 +0530

    changes

--- installtion.sh
+++ installtion.sh
@@ -2,10 +2,18 @@
 
 USER=$(id -u)
 
-echo "user : $USER"
-
 if [ $USER -ne 0 ]
   then
-    echo "run script with root permissions"
+    echo "Please run this script with root priveleges"
     exit 1
 fi
+
+dnf list installed git
+
+if [ $? -ne  0 ] 
+ then
+  echo "Git is not installed, going to install it.."
+  dnf install git -y
+ else 
+   echo "Git is alreadyt installed, nothing to do it.."
+fi   
