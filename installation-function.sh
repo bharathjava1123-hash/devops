a08ae4edc9f5259e46ec93afb1d3c809cd0e0723
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-03-28 08:52:28 +0530

    changes

---
+++ installation-function.sh
@@ -0,0 +1,41 @@
+#!bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+  then
+    echo "Please run this script with root priveleges"
+    exit 1
+fi
+
+VALIDATE(){
+   if [ $1 -ne 0]
+     then 
+      echo "$2 not success, Please check it..."
+      exit 1
+     else
+      echo "$2 installation success"
+    fi
+}
+
+dnf list installed git
+
+if [ $? -ne  0 ] 
+ then
+  echo "Git is not installed, going to install it.."
+  dnf install git -y
+  VALIDATE $? "git installion" 
+ else 
+   echo "Git is already installed, nothing to do it.."
+fi   
+
+dnf list installed mysql
+
+if [ $? -ne 0 ] 
+ then
+  echo "mysql is not installed, going to install it.."
+  dnf install mysql-server -y
+  VALIDATE $? "mysql installion"
+ else 
+  echo "mysql is already installed, nothing to do it..."
+fi
